3519a9468ade51c67e6b4e211407c72cd4c9d51d
unknown <daffatabianoo@gmail.com>
2024-09-08 10:42:59 +0700

    feat(experiences): add experiences card features

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,6 +3,56 @@ import { styles } from "../styles";
 import { motion } from "framer-motion";
 import { textVariant } from "../utils/motion";
 
+import "react-vertical-timeline-component/style.min.css";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import { experiences } from "../constants";
+
+const ExperienceCard = (prop) => {
+    const { experience } = prop
+    
+    return (
+      <VerticalTimelineElement
+        contentStyle={{
+          background: "#ffffff",
+          color: "#ffffff",
+        }}
+        contentArrowStyle={{ borderRight: "7px solid  #ffffff" }}
+        date={experience.date}
+        iconStyle={{ background: experience.iconBg }}
+        icon={
+          <div className='flex justify-center items-center w-full h-full'>
+            <img
+              src={experience.icon}
+              alt={experience.company_name}
+              className='w-[60%] h-[60%] object-contain'
+            />
+          </div>
+        }
+      >
+        <div>
+          <h3 className='text-amber-800 text-[24px] font-bold'>{experience.title}</h3>
+          <p
+            className='text-amber-600 text-[16px] font-semibold'
+            style={{ margin: 0 }}
+          >
+            {experience.company_name}
+          </p>
+        </div>
+  
+        <ul className='mt-5 list-disc ml-5 space-y-2'>
+          {experience.points.map((point, index) => (
+            <li
+              key={`experience-point-${index}`}
+              className='text-amber-600 text-[14px] pl-1 tracking-wider'
+            >
+              {point}
+            </li>
+          ))}
+        </ul>
+      </VerticalTimelineElement>
+    );
+  };
+
 const Experience = () => {
     return(
         <>
@@ -17,7 +67,14 @@ const Experience = () => {
 
         
         <div className='mt-20 flex flex-col bg-yellow-600 rounded-md'>
-          
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+            />
+          ))}
+        </VerticalTimeline>
         </div>
       
       </>
