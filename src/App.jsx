04be1ecec982fd310adb11f0784242d64ccbefdd
unknown <daffatabianoo@gmail.com>
2024-09-08 10:24:21 +0700

    feat(App): Experiences features

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import { Hero } from "./components"
 import About from "./components/About"
+import Experience from "./components/Experience"
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Hero />
         </div>
         <About/>
+        <Experience/>
    </div>
    </BrowserRouter>
   )
