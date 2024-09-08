2bf32ee57fd8b9b216370ddf6f11d7500f02230a
unknown <daffatabianoo@gmail.com>
2024-09-08 21:10:18 +0700

    feat(comps): feature functional add for sending email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,63 @@
 import { StarWrapper } from '../hoc';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: 'Daffa Tabiano',
+          from_email: form.email,
+          to_email: 'daffatabianoo@gmail.com',
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (res) => {
+          console.log(res);
+          setLoading(false);
+          alert('Thank you. I will get back to you as soon as possible.');
+
+          setForm({
+            name: '',
+            email: '',
+            message: '',
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert('Ahh, something went wrong. Please try again.');
+        }
+      );
+  };
+
   return (
     <div
       className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
