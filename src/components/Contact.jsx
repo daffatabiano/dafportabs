babb71b2290fadd833604015b3018e1e995e45b9
unknown <daffatabianoo@gmail.com>
2024-09-08 23:04:38 +0700

    feat(comps): contact spiderparticles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -127,9 +127,7 @@ const Contact = () => {
 
       <motion.div
         variants={slideIn('right', 'tween', 0.2, 1)}
-        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
-        {/* <EarthCanvas /> */}
-      </motion.div>
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"></motion.div>
     </div>
   );
 };
