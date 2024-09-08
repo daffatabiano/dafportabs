6ed33720578eb28d6c939576d1c79d02e684a8c7
unknown <daffatabianoo@gmail.com>
2024-09-08 21:08:01 +0700

    add(comps): Contactss

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+import { StarWrapper } from '../hoc';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+    </div>
+  );
+};
+export default StarWrapper(Contact, 'contact');
