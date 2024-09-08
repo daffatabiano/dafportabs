616b120509e1d5afd95940465bef4348119e6b1f
unknown <daffatabianoo@gmail.com>
2024-09-08 10:03:14 +0700

    feat(comps/styles): components about features, add styles constants

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,28 @@
 import { StarWrapper } from "../hoc"
+import { fadeIn, textVariant } from "../utils/motion"
+import { motion } from "framer-motion"
+import styles from "../styles"
 
 const About = () => {
     return (
-        <div>About</div>
+        <>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Introduction</p>
+          <h2 className={styles.sectionHeadText}>Summary.</h2>
+        </motion.div>
+  
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-4 text-stone-900 text-[17px] max-w-3xl leading-[30px]'
+        >
+          I&apos;m a skilled software developer with experience in TypeScript and
+          JavaScript, and expertise in frameworks like React, Next.js, Redux , Node.js, and
+          Three.js. I&apos;m a quick learner and collaborate closely with clients to
+          create efficient, scalable, and user-friendly solutions that solve
+          real-world problems. Let&apos;s work together to bring your ideas to life!
+        </motion.p>
+  
+      </>
     )
     
 }
--- src/styles.js
+++ src/styles.js
@@ -5,5 +5,9 @@ const styles = {
     "font-black text-stone-900 lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
   heroSubText:
     "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+    sectionHeadText:
+    "text-stone-800 font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+  sectionSubText:
+    "sm:text-[18px] text-[14px] from-amber-600 via-yellow-100 to-amber-800 bg-gradient-to-r bg-clip-text text-transparent uppercase tracking-wider",
 }
 export { styles }
