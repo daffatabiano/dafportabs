8208c358a7cd3c1fc4f4e93cb1a092114d702ace
unknown <daffatabianoo@gmail.com>
2024-09-08 11:09:06 +0700

    feat(comps): tech rendering ball canvaser

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,15 @@
+import { technologies } from "../constants"
 import { StarWrapper } from "../hoc"
+import { BallCanvas } from "./canvas/Ball"
 
 const Tech = () => {
     return(
         <div className='flex flex-row flex-wrap justify-center gap-10'>
-            <h1>
-            Tech Stack
-                </h1>
+               {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
       </div>
     )
 }
