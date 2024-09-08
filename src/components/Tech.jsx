2ccfdc261f01ab18a16a5b93bebb9a4068d44483
unknown <daffatabianoo@gmail.com>
2024-09-08 10:48:23 +0700

    add(comps): add features tech

---
+++ src/components/Tech.jsx
@@ -0,0 +1,10 @@
+import { StarWrapper } from "../hoc"
+
+const Tech = () => {
+    return(
+        <div className='flex flex-row flex-wrap justify-center gap-10'>
+            Tech Stack
+      </div>
+    )
+}
+export default StarWrapper(Tech, 'tech')
