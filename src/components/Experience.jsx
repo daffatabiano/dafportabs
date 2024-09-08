baf9bccdf44ceec1c1431ee92b3fd0c4b7abba89
unknown <daffatabianoo@gmail.com>
2024-09-08 10:26:44 +0700

    feat(comps): features head experiences

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,22 @@
 import { StarWrapper } from "../hoc"
+import { styles } from "../styles";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
 
 const Experience = () => {
     return(
-        <div>Experience</div>
+        <>
+        <motion.div variants={textVariant()}>
+          <p className={`${styles.sectionSubText} text-center`}>
+            What I have done so far
+          </p>
+          <h2 className={`${styles.sectionHeadText} text-center`}>
+            Work Experience.
+          </h2>
+        </motion.div>
+  
+      
+      </>
     )
 }
 
