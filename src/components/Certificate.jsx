40970dfc12168a90c82be7f458c1558faf496ddb
unknown <daffatabianoo@gmail.com>
2024-09-08 20:59:12 +0700

    feat(comps): certificate components add featuring

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -1,9 +1,17 @@
 import { StarWrapper } from '../hoc';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
 
 const Certificate = () => {
   return (
-    <div>
-      <h1>Certificate</h1>
+    <div className={`mt-12 bg-zinc-100 rounded-[20px]`}>
+      <div
+        className={`bg-zinc-200 rounded-2xl ${styles.padding} min-h-[300px]`}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What i achieved</p>
+          <h2 className={styles.sectionHeadText}>Certificate.</h2>
+        </motion.div>
+      </div>
     </div>
   );
 };
