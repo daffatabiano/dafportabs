45cc58d836a6cb303ab286a614d5c4edd92fa223
unknown <daffatabianoo@gmail.com>
2024-09-08 09:32:08 +0700

    feat(app): navbar and hero layouting merger

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import { Hero } from "./components"
 
 function App() {
 
   return (
    <BrowserRouter>
    <div className="relative z-0 bg-white">
-
+   <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
    </div>
    </BrowserRouter>
   )
