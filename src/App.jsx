858086cc78094234e3e77be5b77ab02138a0074d
unknown <daffatabianoo@gmail.com>
2024-09-08 09:30:14 +0700

    feat(app): BrowserRouter for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
 
   return (
-   <>
+   <BrowserRouter>
    <h1 className="text-3xl font-bold underline text-amber-800"> HELLO WORLD !</h1>
-   </>
+   </BrowserRouter>
   )
 }
 
