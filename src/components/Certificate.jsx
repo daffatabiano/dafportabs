95c0d2f49e9dd2400990b27d59992ddff501b94f
unknown <daffatabianoo@gmail.com>
2024-09-08 20:57:23 +0700

    add(comps): components certificate

---
+++ src/components/Certificate.jsx
@@ -0,0 +1,11 @@
+import { StarWrapper } from '../hoc';
+
+const Certificate = () => {
+  return (
+    <div>
+      <h1>Certificate</h1>
+    </div>
+  );
+};
+
+export default StarWrapper(Certificate, 'certificate');
