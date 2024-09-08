471163a1ca610f6f81b799b74afdab24f0d4cdb7
unknown <daffatabianoo@gmail.com>
2024-09-08 19:55:34 +0700

    add(comps): project add components

---
+++ src/components/Project.jsx
@@ -0,0 +1,11 @@
+import { StarWrapper } from '../hoc';
+
+const Project = () => {
+  return (
+    <div>
+      <h1>Project</h1>
+    </div>
+  );
+};
+
+export default StarWrapper(Project, 'project');
