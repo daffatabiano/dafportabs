de7cb49a2a0b9b6882a3be5fa52f6a56f23b1972
unknown <daffatabianoo@gmail.com>
2024-09-08 19:57:22 +0700

    feat(comps): projects features added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import { BrowserRouter } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import { Hero } from "./components"
-import About from "./components/About"
-import Experience from "./components/Experience"
-import Tech from "./components/Tech"
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Hero } from './components';
+import About from './components/About';
+import Experience from './components/Experience';
+import Tech from './components/Tech';
+import Project from './components/Project';
 
 function App() {
-
   return (
-   <BrowserRouter>
-   <div className="relative z-0 bg-white">
-   <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+    <BrowserRouter>
+      <div className="relative z-0 bg-white">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-   </div>
-   </BrowserRouter>
-  )
+        <About />
+        <Experience />
+        <Tech />
+        <Project />
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
