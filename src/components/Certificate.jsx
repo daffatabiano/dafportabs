2dcff5031e804aa7595a5b970db2cbf59f91e301
unknown <daffatabianoo@gmail.com>
2024-09-08 21:05:02 +0700

    feat(comps/const) : certificate components

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -1,6 +1,43 @@
 import { StarWrapper } from '../hoc';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
+import { certs } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+const CertCards = ({
+  index,
+  testimonial,
+  name,
+  designation,
+  company,
+  image,
+  cert,
+}) => (
+  <motion.div
+    variants={fadeIn('', 'spring', index * 0.5, 0.75)}
+    className="from-amber-600 via-yellow-300 to-amber-800 bg-gradient-to-l p-5 rounded-3xl xs:w-[320px] w-full">
+    <div className="">
+      <img src={cert} className="w-full h-[200px] object-contain" alt="" />
+
+      <div className="mt-7 px-5 flex justify-between items-center gap-1">
+        <div className="flex-1 flex flex-col">
+          <p className="text-white font-medium text-[16px]">
+            <span className="text-amber-900">@</span> {name}
+          </p>
+          <p className="mt-1 text-zinc-200 italic text-[12px]">
+            {designation} with {company}
+          </p>
+        </div>
+
+        <img
+          src={image}
+          alt={`feedback_by-${name}`}
+          className="w-10 h-10 rounded-full object-cover"
+        />
+      </div>
+    </div>
+  </motion.div>
+);
 
 const Certificate = () => {
   return (
@@ -15,7 +52,7 @@ const Certificate = () => {
 
       <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
         {certs.map((cert, index) => (
-          <FeedbackCard key={cert.name} index={index} {...testimonial} />
+          <CertCards key={cert.name} index={index} {...cert} />
         ))}
       </div>
     </div>
