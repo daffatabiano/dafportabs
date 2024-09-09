d1db6b11e20555d38d18810c2cb2cf24883ed8ec
unknown <daffatabianoo@gmail.com>
2024-09-09 13:29:18 +0700

    fix(comps): hero color lines

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-amber-800" />
-          <div className="w-1 sm:h-80 h-40 bg-gradient-to-b from-amber-800 to-transparent" />
+          <div className="w-1 sm:h-80 h-40 bg-gradient-to-b from-amber-800 via-yellow-400 to-transparent" />
         </div>
 
         <div>
