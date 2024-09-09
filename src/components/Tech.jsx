b20b073c774a8c2150e488af342edb0317aa52b9
unknown <daffatabianoo@gmail.com>
2024-09-09 11:39:03 +0700

    fix(comps): technologies size responsive

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,31 +1,43 @@
+import { useEffect, useState } from 'react';
 import { technologies } from '../constants';
 import { StarWrapper } from '../hoc';
 
 const Tech = () => {
+  const [shuffledTechnologies, setShuffledTechnologies] = useState([]);
+
+  useEffect(() => {
+    const shuffleArray = (array, multiplier) => {
+      for (let i = multiplier - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    };
+
+    setShuffledTechnologies(shuffleArray([...technologies], 15));
+  }, [technologies]);
+
+  console.log(shuffledTechnologies);
+
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       <div className="overflow-x-hidden w-full relative">
         <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-white via-transparent to-white z-10" />
-        <div className="flex gap-12 animate-slider">
-          {technologies.map((tech) => {
-            for (let i = 0; i > tech.length; i++) {
-              const j = Math.floor(Math.random() * (i + 1));
-              [tech[i], tech[j]] = [tech[j], tech[i]];
-            }
-
+        <div className="flex gap-6 lg:gap-12 animate-slider">
+          {shuffledTechnologies.map((tech) => {
             return (
               <img
                 key={tech.name}
                 src={tech.icon}
                 alt={tech.name}
-                className="w-32 h-32"
+                className="w-12 h-12 md:w-24 md:h-24 lg:w-32 lg:h-32"
               />
             );
           })}
         </div>
-        <div className="flex flex-row-reverse gap-12 animate-sliderReverse mt-10 ">
+        <div className="flex flex-row-reverse lg:gap-12 gap-6 lg:mt-10 animate-sliderReverse mt-5 ">
           {technologies.map((tech) => {
-            for (let i = 0; i > tech.length; i++) {
+            for (let i = 0; i > 20; i++) {
               const j = Math.floor(Math.random() * (i + 1));
               [technologies[i], technologies[j]] = [
                 technologies[j],
@@ -38,7 +50,7 @@ const Tech = () => {
                 key={tech.name}
                 src={tech.icon}
                 alt={tech.name}
-                className="w-32 h-32"
+                className="w-12 h-12 md:w-24 md:h-24 lg:w-32 lg:h-32"
               />
             );
           })}
